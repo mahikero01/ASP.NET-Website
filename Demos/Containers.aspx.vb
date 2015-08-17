bff1245d911ae9326214e5f5c59a072966244292
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2015-08-17 09:12:49 +0800

    up

---
+++ Demos/Containers.aspx.vb
@@ -0,0 +1,15 @@
+﻿
+Partial Class Demos_Containers
+    Inherits System.Web.UI.Page
+
+    Protected Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+        Panel1.Visible = CheckBox1.Checked
+    End Sub
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+    Protected Sub Wizard1_FinishButtonClick(sender As Object, e As WizardNavigationEventArgs) Handles Wizard1.FinishButtonClick
+        Result.Text = "Your name is " & YourName.Text
+        Result.Text &= "<br /> your favorite language is " & FavoriteLanguage.SelectedValue
+    End Sub
+End Class
